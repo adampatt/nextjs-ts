7f46fe278be1b205903f91cea04ec88d02ae8797
adampatt <adampattison08@gmail.com>
2022-01-28 11:35:14 +0100

    Prettier, Jest, RTL, ESlint

---
+++ pages/_app.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react';
+import { AppProps } from 'next/app';
+
+const MyApp: FC<AppProps> = ({ Component, pageProps }: AppProps) => (
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  <Component {...pageProps} />
+);
+export default MyApp;
